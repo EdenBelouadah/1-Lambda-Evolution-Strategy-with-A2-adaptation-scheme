ce3f30fa29ad77890ed6c5bdcee265e3f0abb44b
EdenBelouadah <32595803+EdenBelouadah@users.noreply.github.com>
2017-10-08 19:57:41 +0200

    correction de qlq fautes

--- A2.py
+++ A2.py
@@ -1,10 +1,9 @@
 import math
 import numpy as np
 
-
+#Defining general parameters of the algorithm
 n=3;
 Lambda=10;
-mu=1; #=size population???
 c=math.sqrt(1/n)
 cr=3/(n+3)
 cu=math.sqrt((2-c)/c)
@@ -13,44 +12,48 @@ Bind=1/(4*n)
 Br=math.sqrt(1/(4*n))
 X1=math.sqrt(2/math.pi)
 XN=math.sqrt(n)*(1-1/(4*n)+1/(21*math.pow(n,2)))
-#####################################################################
-#Defining the population: Pop=[X;fits;s;z;r]
-X=[] #values of the solution x1,...,xn more exactly (x1,sigma_i,z,r1),(x,sigma_i,z,r1)
-s=[] #weighted sum for adaptation of step size
-sr=[] #weighted sum for adaptation of step size
-z=[] # ~N(0,1)
-r=[]
-fits=[]#fitnesses of the solutions
-#####################################################################
 
 #Evolution strategy (1,Lambda) ES with A2 reproduction scheme
-def ES_A2(fun, lbounds, ubounds, budget):
-    #Initial population = Only one parent
+def One_Lambda_ES_A2(fun, lbounds, ubounds, budget):
+    #Initial population = Only one parent X ( mu=1 )
     for j in range(n):
-        P.append(np.random.uniform(lbounds,ubounds))
-    s=0
-    f_P=fitness(fun,P)    
+        X.append(np.random.uniform(lbounds,ubounds))
+    s=0#weighted sum for adaptation step size
+    sr=0 #weighted sum for adaptation of step size sigma_r    
     sigma=[]#vector of n variables :all individual step sizes
     r=[]# a vector of n variables : direction vector used to produce line mutation
     sigma_r=0 #step size for direction 
-    while(f_P>optimum and iter<=budget):#while not happy loop
+    f=fitness(fun,X)
+    while(f>optimum and iter<=budget):#while not happy loop
         #Reproduction (A2)
         for k in range(Lambda):#foreach offspring
             zrk=np.random.normal(0,1,1); #choose a normal distributed random zr 
+            zk=np.random.normal(0,1,n)#vector of n normal. dist. rand. variables for the k-th offspring
             X_Nk=[] #initialize the k-th offspring
-            s_Nk=[] #initialize the weighted sum for adaptation of step size of the k-th offspring
-            zk=np.random.normal(0,1,n)#vector of n normal. dist. rand. variables for the k-th offspring 
-            #X_Ek=X[C_k]# Extract the Xs of the chosen parent from the population  
-            #s_Ek=s[C_k]#Extract the weighted sum for adaptation of step size of chosen parent 
+            s_Nk[k]=0 #initialize the weighted sum for adaptation of step size of the k-th offspring
+            sr_Nk[k]=0
+            sigma_Nk=[]#individual step size for k-th offspring
+            r_Nk=[]
+            sigma_r_Nk=0
+            #foreach variable of the k-th offspring
             for i in range(n):
                 #1. Mutation of the object variables
                 X_Nk[i][0]=P[i]+sigma[i]*zk[i]+sigma_r*zrk*r[i]
                 #2. Adaptation of the individual step sizes
-                s_Nk[i]=(1-c)*s+c*(cu*z[k])
-                X_Nk[i][1]=X_Ek[i][1]*np.exp()*np.exp()        
+                s_Nk[k]=(1-c)*s+c*(cu*z[k])#WHAT IS ZK???????????
+                sigma_Nk[i]=sigma[i][1]*np.exp(B*(np.linalg.norm(s_Nk)-Xn))*np.exp(Bind*(math.fabs(s_Nk[i])-X1))#VERIFY THE FORMULA        
                 #Direction adaptation
-                sr[k]=max((1-c)*sr[C_k]+c*(cu*zrk),0)
-                r=(1-cr)*X_Ek[]*+cr*(-)
+                sr[k]=max((1-c)*sr+c*(cu*zrk),0)
+                r_prime=(1-cr)*sigma_r*r+cr*(X_Nk-P)
+                r_Nk=r_prime/np.linalg.norm(r_prime)#VERIFY THIS ONE
+                sigma_r_Nk=max(sigma_r*np.exp(Br*(math.fabs(sr_Nk)-X1)),1/3*np.linalg.norm(sigma_Nk))
+                
+            #Evaluate the k-th offspring
+            f_Nk=fitness(fun,X_Nk)
+            #Choose the best offspring
+            
+            #The Elder become the best offspring
+            P=X_Nk
                 
     #when we go out from the main loop: testing if we have found the optimum or 
     #we failed to find it with the given budget        
