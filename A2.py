3d6f6f1755735aa8e2b4313aad18fca2a4db7c1d
EdenBelouadah <32595803+EdenBelouadah@users.noreply.github.com>
2017-10-08 19:21:50 +0200

    1/y ES

--- A2.py
+++ A2.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 n=3;
-Lambda=5;
+Lambda=10;
 mu=1; #=size population???
 c=math.sqrt(1/n)
 cr=3/(n+3)
@@ -23,44 +23,35 @@ r=[]
 fits=[]#fitnesses of the solutions
 #####################################################################
 
-#Evolution strategies with A2 reproduction scheme
+#Evolution strategy (1,Lambda) ES with A2 reproduction scheme
 def ES_A2(fun, lbounds, ubounds, budget):
-    best_fit=10000000
-    #Remplir la population initiale
-    for i in range(mu):
-        solution=[]
-        for j in range(n):
-            solution.append(np.random.uniform(lbounds,ubounds))
-        X.append(solution)
-        s.append(0)
-        fits.append(fitness(fun,solution))    
-        if(fits[i]<best_fit):
-            best_solution=i
-    
-     
-    while(fits[best_solution]>optimum and iter<=budget):#while not happy loop
-        #choisir les parents
-        
-        #croisement
-        
+    #Initial population = Only one parent
+    for j in range(n):
+        P.append(np.random.uniform(lbounds,ubounds))
+    s=0
+    f_P=fitness(fun,P)    
+    sigma=[]#vector of n variables :all individual step sizes
+    r=[]# a vector of n variables : direction vector used to produce line mutation
+    sigma_r=0 #step size for direction 
+    while(f_P>optimum and iter<=budget):#while not happy loop
         #Reproduction (A2)
-        for k in range(Lambda):
-        zrk=np.random.normal(0,1,1); #définir un z_r aléatoire
-        X_Nk=[] #initialize the new son (Newer)
-        s_Nk=[] #initialize the weighted sum for adaptation of step size of the Newer
-        C_k=np.random.uniform(0,mu)# choose a parent (Elder)
-        X_Ek=X[C_k]# Extract the Xs of the chosen parent from the population  
-        s_Ek=s[C_k]#Extract the weighted sum for adaptation of step size of chosen parent 
-        for i in range(n):
-            #1. Mutation of the object variables
-            X_Nk[i][0]=X_Ek[i][0]+X_Ek[i][1]*X_Ek[i][2]+*zrk*X_Ek[i][3]
-            #2. Adaptation of the individual step sizes
-            s_Nk[i]=(1-c)*s_Ek+c*(cu*z[k])
-            X_Nk[i][1]=X_Ek[i][1]*np.exp()*np.exp()        
-            #Direction adaptation
-            sr[k]=max((1-c)*sr[C_k]+c*(cu*zrk),0)
-            r=(1-cr)*X_Ek[]*+cr*(-)
-            
+        for k in range(Lambda):#foreach offspring
+            zrk=np.random.normal(0,1,1); #choose a normal distributed random zr 
+            X_Nk=[] #initialize the k-th offspring
+            s_Nk=[] #initialize the weighted sum for adaptation of step size of the k-th offspring
+            zk=np.random.normal(0,1,n)#vector of n normal. dist. rand. variables for the k-th offspring 
+            #X_Ek=X[C_k]# Extract the Xs of the chosen parent from the population  
+            #s_Ek=s[C_k]#Extract the weighted sum for adaptation of step size of chosen parent 
+            for i in range(n):
+                #1. Mutation of the object variables
+                X_Nk[i][0]=P[i]+sigma[i]*zk[i]+sigma_r*zrk*r[i]
+                #2. Adaptation of the individual step sizes
+                s_Nk[i]=(1-c)*s+c*(cu*z[k])
+                X_Nk[i][1]=X_Ek[i][1]*np.exp()*np.exp()        
+                #Direction adaptation
+                sr[k]=max((1-c)*sr[C_k]+c*(cu*zrk),0)
+                r=(1-cr)*X_Ek[]*+cr*(-)
+                
     #when we go out from the main loop: testing if we have found the optimum or 
     #we failed to find it with the given budget        
     if(iter<=budget):
